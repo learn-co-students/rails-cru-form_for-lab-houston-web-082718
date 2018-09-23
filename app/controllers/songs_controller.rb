c86e3018a89c342aff804ad2b7ff7247c80b9e4d
V10LET <violet.wmoon@gmail.com>
2018-09-23 17:16:23 -0500

    khnksf

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,14 @@ class SongsController < ApplicationController
   end
 
   def create
+    puts "params = #{params}"
     @song = Song.new(post_params)
+    artist = Artist.find_or_create_by(name: params[:song][:artist])
+    genre = Genre.find_or_create_by(name: params[:song][:genre])
+    @song.artist = artist
+    artist.save
+    @song.genre = genre
+    genre.save
     @song.save
     redirect_to song_path(@song)
   end
